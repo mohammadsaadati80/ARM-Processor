7fc205c0862ebaf7ecefe5cc8bb9b12d0f8d0af3
SM2A <seyedamin1990@gmail.com>
2022-03-11 19:01:36 +0330

    Module I/O

---
+++ Source/ID_Stage.v
@@ -0,0 +1,7 @@
+module ID_Stage (
+    input clk, rst,
+    input [31:0]PC_in,
+    output [31:0]PC
+);
+    
+endmodule
---
+++ Source/ID_Stage_Reg.v
@@ -0,0 +1,7 @@
+module ID_Stage_Reg (
+    input clk, rst,
+    input [31:0]PC_in,
+    output reg [31:0]PC
+);
+    
+endmodule
