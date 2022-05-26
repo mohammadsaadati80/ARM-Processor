fb4c38e36d03cac5dcbf996300a8d2bd57de32d6
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-05-27 00:06:37 +0430

    Another way of doing this

--- Source/SRAM.v
+++ Source/SRAM.v
@@ -1,14 +1,15 @@
 // This moduls is a mock module to test code in modelsim
 // On real FPGA we use real SRAM instead of this module
-module SRAM(inout [15:0]SRAM_DQ, input [17:0]SRAM_ADDR,
-  input SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N);
+`timescale 1ps/1ps
 
-  wire [8:0] address_t;
-  reg [15:0] data[0:511];
-
-  assign address_t = SRAM_ADDR[8:0];
-  assign SRAM_DQ = SRAM_WE_N ? data[SRAM_ADDR]: 16'bz;
-
-  always @(*) if (SRAM_WE_N == 1'b0) data[address_t] <= SRAM_DQ;
+module SRAM (input clk, rst, sram_we_n, input [17:0]sram_address, inout [15:0]sram_dq);
+  
+  reg [31:0]memory[0:511];
+  
+  assign #10 sram_dq = sram_we_n ? memory[sram_address] : 32'bzzzz_zzzz_zzzz_zzzz_zzzz_zzzz_zzzz_zzzz;
+  
+  always @ (posedge clk) begin
+    if (!sram_we_n) memory[sram_address] = sram_dq;
+  end
 
 endmodule
--- Testbench/Test_Bench.v
+++ Testbench/Test_Bench.v
@@ -5,11 +5,10 @@ module Test_Bench ();
     reg clk = 1, rst = 1;
  
     wire SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N;
-    wire [15:0]sram_dq;
-    wire [17:0]sram_address;
+    wire [15:0] sram_dq;
+    wire [17:0] sram_address;
 
-    // SRAM sram(clk, rst, SRAM_WE_N, sram_address, sram_dq);
-    SRAM sram(sram_dq, sram_address, SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N);
+    SRAM sram(clk, rst, SRAM_WE_N, sram_address, sram_dq);
     ARM test(clk, rst, 1'b0, sram_dq, sram_address, SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N);
 
     always #10 clk = ~clk;
